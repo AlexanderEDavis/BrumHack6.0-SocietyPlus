7f82e99269f2f8303817276fceea3bcacb3db78f
Alexander Davis <contact@alexanderdavis.tech>
2017-03-25 22:33:15 +0000

    Sorry Team
    Think I Fixed IT!

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'alexand2_socplus');
+   define('DB_PASSWORD', 'M&i6_hNkhL}M');
+   define('DB_DATABASE', 'alexand2_societyplus');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
